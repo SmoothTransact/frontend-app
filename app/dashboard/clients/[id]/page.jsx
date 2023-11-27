0e76b48f40fcb149079f241aec691b13b9965f00
paulynson <paulabakada@gmail.com>
2023-11-27 02:13:53 +0100

    Updated Edit and Create clients

--- app/dashboard/clients/[id]/page.jsx
+++ app/dashboard/clients/[id]/page.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import axios from "axios";
+// import React from "react";
+// import axios from "axios";
 
-async function getClient(id) {
-  const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_BASE_URL}clients/${id}`,
-    {
-      next: {
-        revalidate: 60,
-      },
-    }
-  );
-  return res;
-}
+// async function getClient(id) {
+//   const res = await axios.get(
+//     `${process.env.NEXT_PUBLIC_BASE_URL}clients/${id}`,
+//     {
+//       next: {
+//         revalidate: 60,
+//       },
+//     }
+//   );
+//   return res;
+// }
 
-export default async function ClientDetails({ params }) {
-  const client = await getClient(params.id);
+// export default async function ClientDetails({ params }) {
+//   const client = await getClient(params.id);
 
-  console.log(`ID is: , ${process.env.NEXT_PUBLIC_BASE_URL}clients/${client}`);
+//   console.log(`ID is: , ${process.env.NEXT_PUBLIC_BASE_URL}clients/${client}`);
 
-  return (
-    <main className="px-6 py-8">
-      <nav>Client {client.fullName}</nav>
-    </main>
-  );
-}
+//   return (
+//     <main className="px-6 py-8">
+//       <nav>Client {client.fullName}</nav>
+//     </main>
+//   );
+// }
